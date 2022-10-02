717d0d56c894f8dc6cd3a8c59510c5aa9bed7bbb
Thomas Aspinall <tomaspinall2512@gmail.com>
2022-10-03 00:39:16 +1000

    0.2.0.9000 R function add

--- R/fks.SP.R
+++ R/fks.SP.R
@@ -1,4 +1,28 @@
+#' Fast Kalman Smoother Through Sequential Processing:
+#'
+#' @description This function can be run after running \code{\link{fkf.SP}} to produce
+#' "smoothed" estimates of the state variable \eqn{\alpha_t}{alpha(t)}.
+#' Unlike the output of the filter, these estimates are conditional
+#' on the entire set of \eqn{n}{n} data points rather than only the past, see details.
+#'
+#' @param FKF.SP_obj  An S3-object of class "fkf.SP", returned by \code{\link{fkf.SP}}.
+#'
+#' @return An S3-object of class "fks.SP" which is a list with the following elements:
+#'
+#'   \code{ahatt}  A \eqn{m \times n}{m * n}-matrix containing the
+#'   smoothed state variables, i.e. ahatt[,t] = \eqn{a_{t|n}}{a(t|n)}\cr
+#'   \code{Vt}  A \eqn{m \times m \times n}{m * m * n}-array
+#'   containing the variances of \code{ahatt}, i.e. Vt[,,t] = \eqn{P_{t|n}}{P(t|n)}\cr
+#'
+#' @details
+#' temporary
+#'
+#' @section References:
+#'
+#' Koopman, S. J. and Durbin, J. (2000). \emph{Fast filtering and smoothing for multivariate state space models} Journal of Time Series Analysis Vol. 21, No. 3
+#' 
 #'@examples
+#'### Perform Kalman Filtering and Smoothing through sequential processing:
 #'#Nile's annual flow:
 #'yt <- Nile
 #'
